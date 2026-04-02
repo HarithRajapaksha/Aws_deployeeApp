a49f948f988896f535008980d1cd7efde383a144
HarithRajapaksha <priyaharshanaharith@gmail.com>
2026-04-02 12:41:02 +0530

    add all things

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hello, Harith !</h1>
+      <p>Welcome to your React app!</p>
+      <div>
+        Deploye try using this one one AWS Hee heee
+          <div>
+            Hi Hi
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
